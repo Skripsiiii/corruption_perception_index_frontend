786e0772e339c5b2ed476f8506704455150716fd
keygenext <jonathantheresantojob@gmail.com>
2024-04-29 11:13:43 +0700

    Date : 29/04 Jonathan Added : login activity colors.xml

---
+++ app/src/main/java/com/example/corruptionperceptionindex/src/screens/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.corruptionperceptionindex.src.screens.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.corruptionperceptionindex.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
